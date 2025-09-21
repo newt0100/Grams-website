e9cad1213f90444235fa89478793dc44170efd1a
gram <grampropst@icloud.com>
2025-09-21 15:27:50 -0600

    doing some docker shit

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-docker run --rm -p 8080:80 --name html_server `
-    -v "${PWD}\nginx:/etc/nginx/conf.d/" `
-    -v "${PWD}:/usr/share/nginx/html:ro" `
+#!/bin/bash
+
+docker run --rm -p 8080:80 --name gram_web \
+    -v "${PWD}/nginx:/etc/nginx/conf.d/" \
+    -v "${PWD}:/usr/share/nginx/html:ro" \
     nginx
