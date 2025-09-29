ccf10686e0a2cd884f73ff58aa6bd173c821b1b3
gram <grampropst@icloud.com>
2025-09-28 20:02:48 -0600

    IT FUCKING WORKS

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run --rm -p 8080:80 --name gram_web \
-    -v "${PWD}/nginx:/etc/nginx/conf.d/" \
-    -v "${PWD}:/usr/share/nginx/html:ro" \
+    -v "C:/Users/gramp/Desktop/Game projects/gram_web/nginx/:/etc/nginx/conf.d/" \
+    -v "C:/Users/gramp/Desktop/Game projects/gram_web/:/usr/share/nginx/html:ro" \
     nginx
